52f8443abf917cdb8e37b55329b1ce3835217594
nugraha-ga <alditiya.nugraha1@gmail.com>
2022-01-29 18:05:30 +0700

    fixing

--- page/absen-pegawai.php
+++ page/absen-pegawai.php
@@ -11,6 +11,80 @@
 
 <body>
     <?php include_once('../component/functions.php') ?>
+    <script>
+        $('div.box-login').css('display','none');
+        let checkLocationBlock = false;
+
+        //check block posisi
+        navigator.geolocation.watchPosition(function(position) {
+                checkLocationBlock = true;
+            },
+            function(error) {
+                if (error.code == error.PERMISSION_DENIED) {
+                    checkLocationBlock = false;
+                    errorRedirectMessage('Izin Lokasi diblock, Silahkan buka akses perizinan lokasi pada website ini', document.URL);
+                }
+            });
+
+        let theUrl = new URL(document.URL);
+        let findLongitude = theUrl.searchParams.get("longitude");
+        let findLatitude = theUrl.searchParams.get("latitude");
+
+        //cek jika parameter longitude dan latitude tidak ada
+        function updateLongLat(uri, lat, long) {
+            let firstUrl = updateURLParameter(uri, 'longitude', long);
+            let finalurl = updateURLParameter(firstUrl, 'latitude', lat);
+            window.location.href = finalurl;
+        };
+
+        function checkJikaParameterTidakAda(urifor) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                /* get lokasi user */
+                let lat = position.coords.latitude;
+                let long = position.coords.longitude;
+                updateLongLat(urifor, lat, long);
+            });
+        }
+
+        //cek jika lokasi tidak sesuai
+        function updateLongLatWithAlert(urlnyaw) {
+            errorRedirectMessage('Lokasi Tidak Sesuai', urlnyaw);
+        };
+
+        function checkJikaParameterTidakSesuai(urlnya) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                /* get lokasi user */
+                let newLat = position.coords.latitude;
+                let newLong = position.coords.longitude;
+
+                // console.log(findLongitude + ' == ' + newLong);
+                // console.log(findLatitude + ' == ' + newLat);
+                if (findLongitude != newLong && findLatitude != newLat) {
+                    firstUrl = updateURLParameter(urlnya, 'longitude', newLong);
+                    finalurl = updateURLParameter(firstUrl, 'latitude', newLat);
+                    updateLongLatWithAlert(finalurl);
+                    console.log('beda');
+                } else {
+                    console.log('sama');
+                }
+            });
+        }
+
+
+        if (findLongitude == null && findLatitude == null) {
+            checkJikaParameterTidakAda(document.URL);
+        } else {
+            console.log('passed 1');
+            $('div.box-login').css('display','block');
+        }
+
+        if (findLongitude != null && findLatitude != null) {
+            checkJikaParameterTidakSesuai(document.URL);
+        } else {
+            console.log('passed 2');
+            $('div.box-login').css('display','block');
+        }
+    </script>
     <div class="header">DaraWeb.</div>
     <div class="box-login">
         <div class="title-login">
@@ -28,11 +102,11 @@
                     if ($res_lok) {
                         if ($res_lok->num_rows == 1) {
                             $lok = $res_lok->fetch_assoc();
-                            if ($lok['longitude'] == '106.8302336' && $lok['latitude'] == '-6.22592') {
+                            if ($lok['longitude'] == $_GET['longitude'] && $lok['latitude'] == $_GET['latitude']) {
                                 echo "Silahkan Scan QR Code untuk melakukan Absen <br><br>";
                                 echo '<img src="https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=200x200" alt="" title="Scan QR Code!" />';
                             } else {
-                                echo '<script type="text/javascript">', 'errorRedirectMessage("Pastikan lokasi Anda berada di Toko. Silahkan coba kembali.", "beranda-pegawai.php");', '</script>';
+                                echo '<script type="text/javascript">', 'errorRedirectMessage("Pastikan lokasi Anda berada di Toko. Tunggu Beberapa Saat atau Klik Tombol OK.", document.location.origin + "/daraweb/page/absen-pegawai.php");', '</script>';
                             }
                         }
                     }
@@ -43,13 +117,6 @@
             ?>
         </div>
     </div>
-    <!-- <script>
-        navigator.geolocation.getCurrentPosition(function(position) {
-            /* get lokasi user */
-            let lat = position.coords.latitude;
-            let long = position.coords.longitude;
-        });
-    </script> -->
 </body>
 
 </html>
